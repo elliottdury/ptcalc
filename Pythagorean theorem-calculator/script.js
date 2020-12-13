9d881e3bee04f81385864c24b51672c6b4574a4c
elliott <elliottdury06@gmail.com>
2020-12-13 11:24:36 +0100

    removed

--- Pythagorean theorem-calculator/script.js
+++
@@ -1,8 +0,0 @@
-function formhandling() {
-var result = document.getElementById("result")
-var sidea = document.forms["form"]["sidea"].value;
-var sideb = document.forms["form"]["sideb"].value;
-
-   result.innerHTML = "hypotenuse = " + Math.sqrt(Math.pow(sidea, 2)+Math.pow(sideb, 2));
-    return false;
-  }
